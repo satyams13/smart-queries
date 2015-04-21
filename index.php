7a97d2f2d3f60dfb3961106ff5406a4933224f71
Rajender Thakur <rthakur.dev@gmail.com>
2015-04-21 13:10:19 +0530

    Updated Difference Between Where and Having mysql Query.

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
 <?php
-//Queries to output the value as a,b,c from multiple rows in mysql
+//Query to output the value as a,b,c from multiple rows in mysql
  SELECT GROUP_CONCAT(column_name) as colname FROM `tablename` ORDER BY `column` DESC 
+
+//Difference Between having and WHERE clause mysql Query
+	- HAVING is used to check conditions after the aggregation takes place.
+	For Example:
+		select City, CNT=Count(1)
+		From Address
+		Where State = 'MA'
+		Group By City
+		Having Count(1)>5
+	
+	
+	- WHERE is used before the aggregation takes place.
+	For Example:
+		select City, CNT=Count(1)
+		From Address
+		Where State = 'MA'
+		Group By City
+
 ?>
+
+
